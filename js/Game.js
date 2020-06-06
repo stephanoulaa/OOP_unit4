305e35da0d966bb2bf9a8bb5561da06b49a9aeda
Stephany Prodromos <stephprodromos@gmail.com>
2020-06-06 18:40:59 -0500

    getting set up

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,13 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game {
+    constructor() {
+        // missed starts as 0 b/c no guesses have been made yet
+        this.missed = 0;
+        // phrases is list of phrases for the game
+        this.phrases = ['I Love You', 'You Rock', 'Be Yourself', 'Everything in Moderation', 'Normal is Boring'];
+        // activePhrase represents Phrase obj currently in play
+        this.activePhrase = null;
+    }
+}
